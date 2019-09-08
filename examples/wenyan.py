a9ab7a7508e6e9c320b18c70d4d273e37b43cd92
Meowcolm024 <luoyicong1026@live.com>
2019-09-08 17:57:27 +0800

    updated README

--- examples/wenyan.py
+++ examples/wenyan.py
@@ -1,39 +1,40 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
-### var0
-
-#  此乃批注也
-
-a = 1  
-b = 2  
-var0 = True  
+### var2 var1 var0
 
 """ 
-滑稽列传 
+此乃
+多行注释也
 """
+
+# 此乃批注也
+
+var1 = 1  
+var0 = 2  
+var2 = True  
 
 def add(x,y) : 
     return x + y
 
-print( add(a,b) )
+print( add( var1 , var0 ) )
 
 for i in [1,2,3] : 
     print( i )
 
-while a < 3 :
-    a = a + 1  
+while var1 < 3 :
+    var1 = var1 + 1  
     print( " 善哉 " )
 
-if b == 3 :
+if var0 == 3 :
     print( " 行路难 " )
-elif b < 0 :
+elif var0 < 0 :
     print( " 多歧路 " )
 else :
     print( " 今安在 " )
 
-if var0 == True :
-    var0 = False  
+if var2 == True :
+    var2 = False  
 
-print( var0 )
+print( var2 )
 
